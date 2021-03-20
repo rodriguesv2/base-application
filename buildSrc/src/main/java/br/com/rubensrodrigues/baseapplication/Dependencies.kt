a53e0b429205ff09ea3a2cbf5b8a11362e631a75
Rubens Rodrigues <rubens.rodrigues@mobile2you.com.br>
2021-03-20 00:18:05 -0300

    added dependecies

--- buildSrc/src/main/java/br/com/rubensrodrigues/baseapplication/Dependencies.kt
+++ buildSrc/src/main/java/br/com/rubensrodrigues/baseapplication/Dependencies.kt
@@ -2,6 +2,13 @@ package br.com.rubensrodrigues.baseapplication
 
 object Dependencies {
 
+    object Classpath {
+        const val gradle = "com.android.tools.build:gradle:${Version.gradle}"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Version.kotlin}"
+        const val navigationSafeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Version.navigation}"
+        const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Version.hilt}"
+    }
+
     object Language {
         const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlin}"
     }
@@ -13,6 +20,28 @@ object Dependencies {
     object UI {
         const val appCompat = "androidx.appcompat:appcompat:${Version.appCompat}"
         const val material = "com.google.android.material:material:${Version.material}"
-        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Version.constraintLayout}"
+        const val constraintLayout =
+            "androidx.constraintlayout:constraintlayout:${Version.constraintLayout}"
+        const val recyclerView = "androidx.recyclerview:recyclerview:${Version.recyclerView}"
+        const val glide = "com.github.bumptech.glide:glide:${Version.glide}"
+    }
+
+    object Architecture {
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Version.lifecycle}"
+        const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Version.lifecycle}"
+        const val runtime = "androidx.lifecycle:lifecycle-runtime-ktx:${Version.lifecycle}"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Version.coroutines}"
+        const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Version.navigation}"
+        const val navigation = "androidx.navigation:navigation-ui-ktx:${Version.navigation}"
+        const val hilt = "com.google.dagger:hilt-android:${Version.hilt}"
+    }
+
+    object Networking {
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Version.retrofit}"
+        const val gson = "com.google.code.gson:gson:${Version.gson}"
+    }
+
+    object Kapt {
+        const val hilt = "com.google.dagger:hilt-android-compiler:${Version.hilt}"
     }
 }
--- buildSrc/src/main/java/br/com/rubensrodrigues/baseapplication/Version.kt
+++ buildSrc/src/main/java/br/com/rubensrodrigues/baseapplication/Version.kt
@@ -5,8 +5,22 @@ object Version {
 
     const val core = "1.3.2"
 
+    const val gradle = "4.1.2"
+
     //UI
     const val appCompat = "1.2.0"
     const val material = "1.2.1"
     const val constraintLayout = "2.0.4"
+    const val recyclerView = "1.1.0"
+    const val glide = "4.12.0"
+
+    //Architecture
+    const val lifecycle = "2.3.0"
+    const val coroutines = "1.3.0"
+    const val navigation = "2.3.3"
+    const val hilt = "2.33-beta"
+
+    //Networking
+    const val retrofit = "2.9.0"
+    const val gson = "2.8.6"
 }
