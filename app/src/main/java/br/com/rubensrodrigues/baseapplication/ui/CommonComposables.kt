1df9727097815ef1980f4c72d3700f4c2ffc2a7c
Rubens Rodrigues <rubens.rodrigues@mobile2you.com.br>
2021-08-10 11:40:15 -0300

    fix: (Adjusted card size)

--- app/src/main/java/br/com/rubensrodrigues/baseapplication/ui/CommonComposables.kt
+++ app/src/main/java/br/com/rubensrodrigues/baseapplication/ui/CommonComposables.kt
@@ -1,14 +1,14 @@
 package br.com.rubensrodrigues.baseapplication.ui
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,15 +20,18 @@ fun MovieListCard() {
         modifier = Modifier
             .padding(vertical = 8.dp)
             .padding(start = 8.dp)
+            .wrapContentSize()
     ) {
         Image(
             painter = painterResource(id = R.drawable.movie_or_serie_images),
             contentDescription = "",
+            modifier = Modifier
+                .height(120.dp),
+            contentScale = ContentScale.FillHeight
         )
     }
 }
 
-@Preview
 @Composable
 fun MovieList() {
     LazyRow(
@@ -38,10 +41,4 @@ fun MovieList() {
             MovieListCard()
         }
     }
-}
-
-@Preview
-@Composable
-fun PreviewMovieListCard() {
-    MovieListCard()
 }
